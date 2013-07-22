4c00f8ec4f77deff7f7be994e18ea95ff0b296d2
minorandres <minorandres@gmail.com>
2013-07-22 15:16:02 -0600

    cambios al contrato matricula
    se cambio las variables estaticas compromisos y responsabilidades

--- ExS_Models/src/exs/mod/var/Matricula_Var.java
+++ ExS_Models/src/exs/mod/var/Matricula_Var.java
@@ -18,30 +18,45 @@ public class Matricula_Var {
             + "\t\t--ExS-- ";
     //Contrato
     public static String RESPONSABILIDADES = 
-            "Tener el apoyo de una tutoría en el curso que lo requiere durante todo ciclo lectivo; que consiste en un espacio de 2 horas \n"
-            + " semanales para la revisión y práctica de conceptos claves del curso."
-            + "\n\n Recibir seguimiento académico y  personal por parte del equipo de Orientación del Programa Éxito Académico."
-            + "\n\n Participar de talleres en relación con temas psicoeducativos. "
-            //+ "\n\n Participar de talleres en relación con temas psicoeducativos.\n"
-            ;
-    public static String COMPROMISOS = 
-            " Estar matriculado actualmente en el curso que requiere el apoyo de la tutoría. "
-            + "\n\n Asistir a la hora de consulta del profesor del curso. "
-            + "\n\n Estudiar mínimo 5 horas de manera independiente previo a la tutoría."
-            + "\n\n Responsabilizarse de  asistir a todas las tutorías y ser  puntual."
-            + "\n\n Realizar semanalmente las tareas formativas asignadas en las tutorías."
-            + "\n\n Estar dispuesto(a) a ceder su espacio en la tutoría si se ausenta de las mismas más de dos veces sin justificar."
-             + "\n\n Firmar la lista asistencia todas las semanas.";
-    public static String RESPONSABILIDADES_F = " Tener el apoyo tutorial durante todo \nun ciclo lectivo."
-            + "\n\n Asistir a tutoría grupal como individual."
-            + "\n\n Realizar un diagnóstico psicopedagógico \nsobre: técnicas de estudio, estilos de \naprendizaje, hábitos de estudio, \nentre otros."
-            + "\n\n Recibir retroalimentación referente \nal diagnóstico."
-            + "\n\n Participar de talleres \nen relación con temas \npsicoeducativos.\n";
-    public static String COMPROMISOS_F = " Llenar la boleta de inscripción."
-            + "\n\n Responsabilizarse de asistir \npuntual y frecuentemente a \nlas tutorías."
-            + "\n\n Estudiar previo a la tutoría."
-            + "\n\n Mantener comunicación constante \ncon los(las) tutores(as) \ny justificar las ausencias."
-            + "\n\n Estar dispuesto(a) a ceder su \nespacio en la tutoría si se ausenta \nde las mismas más de DOS veces \nsin justificar."
-            + "\n\n Firmar la lista de asistencia \ntodas las semanas.\n";
+            "Tener el apoyo de una tutoría en el curso que lo \n requiere "
+            + " durante todo ciclo lectivo; que consiste \n en un espacio "
+            + " de 2 horas semanales para  la \n revisión y práctica "
+            + "de conceptos claves del curso."
+            + "\n\n Recibir seguimiento académico y  personal por \n parte "
+            + "del equipo de Orientación del \n Programa Éxito Académico. "
+            + "\n\n Participar de talleres en relación con temas \n  psicoeducativos. ";
+    public static String COMPROMISOS = " Estar matriculado actualmente en el curso \n"
+            + " que requiere el apoyo de la tutoría. "
+            + "\n\n Asistir a la hora de consulta \n del profesor del curso. "
+            + "\n\n Estudiar mínimo 5 horas de manera \n independiente "
+            + "previo a la tutoría."
+            + "\n\n Responsabilizarse de  asistir a todas las \n tutorías "
+            + " y ser  puntual."
+            + "\n\n Realizar semanalmente las tareas formativas \n"
+            + " asignadas en las tutorías."
+            + "\n\n Estar dispuesto(a) a ceder su espacio \n en la tutoría "
+            + " si se ausenta de las mismas más de \n dos veces sin justificar."
+            + "\n\n Firmar la lista asistencia todas las semanas.\n";
+    public static String RESPONSABILIDADES_F =  
+            "Tener el apoyo de una tutoría en el curso que lo \n requiere "
+            + " durante todo ciclo lectivo; que consiste \n en un espacio "
+            + " de 2 horas semanales para  la \n revisión y práctica "
+            + "de conceptos claves del curso."
+            + "\n\n Recibir seguimiento académico y  personal por \n parte "
+            + "del equipo de Orientación del \n Programa Éxito Académico. "
+            + "\n\n Participar de talleres en relación con temas \n  psicoeducativos. ";
+    public static String COMPROMISOS_F = 
+          " Estar matriculado actualmente en el curso \n"
+            + " que requiere el apoyo de la tutoría. "
+            + "\n\n Asistir a la hora de consulta \n del profesor del curso. "
+            + "\n\n Estudiar mínimo 5 horas de manera \n independiente "
+            + "previo a la tutoría."
+            + "\n\n Responsabilizarse de  asistir a todas las \n tutorías "
+            + " y ser  puntual."
+            + "\n\n Realizar semanalmente las tareas formativas \n"
+            + " asignadas en las tutorías."
+            + "\n\n Estar dispuesto(a) a ceder su espacio \n en la tutoría "
+            + " si se ausenta de las mismas más de \n dos veces sin justificar."
+            + "\n\n Firmar la lista asistencia todas las semanas.\n";
     public static String CONTRATO = "Yo %s identificación %s, estudiante del curso %s, de la carrera de %s, tengo pleno conocimiento de los derechos y compromisos que adquiero al inscribirme en las Tutorías del Programa Éxito Académico en la UNA. Al haber aceptado este contrato estoy dando un paso importante en mi vida porque al cumplirlo estaré preparado(a) para continuar con mi siguiente meta y comprobar que tengo control de mis estudios. ";
 }
